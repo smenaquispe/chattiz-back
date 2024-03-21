58ab66f4d2236f2adf2f9d6bb2fc3acb03463f05
smena <sergiosantosmenaq@gmail.com>
2024-03-21 15:44:27 -0500

    new migration

--- Migrations/20240321204333_UpdateModels.Designer.cs
+++ Migrations/20240321204333_UpdateModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using chattiz_back.Data;
 
@@ -11,9 +12,11 @@ using chattiz_back.Data;
 namespace chattiz_back.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321204333_UpdateModels")]
+    partial class UpdateModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240321204333_UpdateModels.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chattiz_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastMessager",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfMessages",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Chats");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastMessager",
+                table: "ChatUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfMessages",
+                table: "ChatUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ChatUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastMessager",
+                table: "ChatUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfMessages",
+                table: "ChatUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ChatUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastMessager",
+                table: "Chats",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfMessages",
+                table: "Chats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Chats",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
